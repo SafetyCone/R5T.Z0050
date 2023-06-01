e4e8ca608726a1707676881ebab9b6728314c9d8
davidcoats <david@getsafetycone.com>
2023-06-01 09:49:26 -0700

    Initial commit.

---
+++ source/R5T.Z0050/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.Z0050
+{
+	/// <summary>
+	/// Package reference values.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.Z0050/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace R5T.Z0050
+{
+    public static class Instances
+    {
+		
+    }
+}
