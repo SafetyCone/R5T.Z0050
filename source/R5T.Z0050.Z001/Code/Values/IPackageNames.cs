dadd9da758914e28c45a41c24b6c31fe3917b728
davidcoats <david@getsafetycone.com>
2024-04-01 18:12:03 -0700

    Interim changes.

--- source/R5T.Z0050.Z001/Code/Values/IPackageNames.cs
+++ source/R5T.Z0050.Z001/Code/Values/IPackageNames.cs
@@ -9,8 +9,18 @@ namespace R5T.Z0050.Z001
     public partial interface IPackageNames : IValuesMarker
     {
         /// <summary>
+        /// <para><value>Microsoft.AspNetCore.Components.Web</value></para>
+        /// </summary>
+        public string Microsoft_AspNetCore_Components_Web => "Microsoft.AspNetCore.Components.Web";
+
+        /// <summary>
         /// <para><value>Microsoft.AspNetCore.Components.WebAssembly</value></para>
         /// </summary>
         public string Microsoft_AspNetCore_Components_WebAssembly => "Microsoft.AspNetCore.Components.WebAssembly";
+
+        /// <summary>
+        /// <para><value>Microsoft.AspNetCore.Components.WebAssembly.Server</value></para>
+        /// </summary>
+        public string Microsoft_AspNetCore_Components_WebAssembly_Server => "Microsoft.AspNetCore.Components.WebAssembly.Server";
     }
 }
--- source/R5T.Z0050.Z001/Code/Values/IPackageReferences.cs
+++ source/R5T.Z0050.Z001/Code/Values/IPackageReferences.cs
@@ -10,12 +10,39 @@ namespace R5T.Z0050.Z001
     public partial interface IPackageReferences : IValuesMarker
     {
         /// <summary>
+        /// <para><value><inheritdoc cref="IPackageNames.Microsoft_AspNetCore_Components_Web" path="/descendant::value"/>, 8.0.0</value></para>
+        /// </summary>
+        public PackageReference Microsoft_AspNetCore_Components_Web_8_0_1 => new PackageReference
+        {
+            Name = Instances.PackageNames.Microsoft_AspNetCore_Components_Web,
+            Version = new Version(8, 0, 1),
+        };
+
+        /// <summary>
         /// <para><value><inheritdoc cref="IPackageNames.Microsoft_AspNetCore_Components_WebAssembly" path="/descendant::value"/>, 8.0.0</value></para>
         /// </summary>
         public PackageReference Microsoft_AspNetCore_Components_WebAssembly_8_0_0 => new PackageReference
         {
             Name = Instances.PackageNames.Microsoft_AspNetCore_Components_WebAssembly,
             Version = new Version(8, 0, 0),
+        };
+
+        /// <summary>
+        /// <para><value><inheritdoc cref="IPackageNames.Microsoft_AspNetCore_Components_WebAssembly" path="/descendant::value"/>, 8.0.1</value></para>
+        /// </summary>
+        public PackageReference Microsoft_AspNetCore_Components_WebAssembly_8_0_1 => new PackageReference
+        {
+            Name = Instances.PackageNames.Microsoft_AspNetCore_Components_WebAssembly,
+            Version = new Version(8, 0, 1),
+        };
+
+        /// <summary>
+        /// <para><value><inheritdoc cref="IPackageNames.Microsoft_AspNetCore_Components_WebAssembly_Server" path="/descendant::value"/>, 8.0.1</value></para>
+        /// </summary>
+        public PackageReference Microsoft_AspNetCore_Components_WebAssembly_Server_8_0_1 => new PackageReference
+        {
+            Name = Instances.PackageNames.Microsoft_AspNetCore_Components_WebAssembly_Server,
+            Version = new Version(8, 0, 1),
         };
     }
 }
